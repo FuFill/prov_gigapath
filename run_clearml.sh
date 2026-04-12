23264799d5ca344a75050edbcf01eacb7d61b9e7
FuFill <chelek228@gmail.com>
2026-04-12 15:35:27 +0300

    Update ClearML docs and run script

--- run_clearml.sh
+++ run_clearml.sh
@@ -13,7 +13,7 @@ PROJECT="pershin-medailab"
 NAME="Pathomorphology"
 QUEUE="default"
 DOCKER_IMAGE="pytorch/pytorch:2.8.0-cuda12.9-cudnn9-runtime"
-REPO_URL="https://code.university.innopolis.ru/AI/Лаборатория Ильи Першина/_git/prov-gigapath"
+REPO_URL="https://code.university.innopolis.ru/AI/%D0%9B%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%B8%D1%8F%20%D0%98%D0%BB%D1%8C%D0%B8%20%D0%9F%D0%B5%D1%80%D1%88%D0%B8%D0%BD%D0%B0/_git/prov-gigapath"
 
 MAX_SLIDES=""
 GPU_FLAG=""
